41117633181d53302683c6a3b227e90021e78444
Felix Sun <sunfmin@gmail.com>
2009-05-11 10:55:07 +0800

    remove django, simplify the app

--- main.py
+++ main.py
@@ -1,45 +1,110 @@
-import logging, os, sys
+import os
 
-# Google App Engine imports.
-from google.appengine.ext.webapp import util
 from google.appengine.api import users
+from google.appengine.ext import db
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import login_required
+from google.appengine.ext.webapp.util import run_wsgi_app
 
-# Remove the standard version of Django.
-for k in [k for k in sys.modules if k.startswith('django')]:
-  del sys.modules[k]
+from models import *
 
-# Force sys.path to have our own directory first, in case we want to import
-# from it.
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+template.register_template_library('templatefilters')
 
-# Must set this env var *before* importing any part of Django
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+_DEBUG = True
+class BaseRequestHandler(webapp.RequestHandler):
+    """Supplies a common template generation function.
 
-import django.core.handlers.wsgi
-import django.core.signals
-import django.db
-import django.dispatch.dispatcher
+    When you call generate(), we augment the template variables supplied with
+    the current user in the 'user' variable and the current webapp request
+    in the 'request' variable.
+    """
+    def render(self, template_name, template_values={}):
+        values = {
+            'request': self.request,
+            'user': users.get_current_user(),
+            'login_url': users.create_login_url(self.request.uri),
+            'logout_url': users.create_logout_url('http://%s/' % (
+                self.request.host,)),
+            'debug': self.request.get('deb'),
+            'application_name': 'Task Manager',}
+        values.update(template_values)
+        directory = os.path.dirname(__file__)
+        path = os.path.join(directory, os.path.join('templates', template_name))
+        self.response.out.write(template.render(path, values, debug=_DEBUG))
 
-def log_exception(*args, **kwds):
- logging.exception('Exception in request:')
+class MyHome(BaseRequestHandler):
+    @login_required
+    def get(self):
+        quests = Quest.all().filter("creator = ", users.get_current_user()).order("-updated_at").fetch(100)
+        self.render('myhome.html', {"quests": quests})
 
-# Log errors.
-django.dispatch.dispatcher.connect(
-   log_exception, django.core.signals.got_request_exception)
+class Ask(BaseRequestHandler):
+    @login_required
+    def get(self):
+        word_name = self.request.GET.get('q', None)
+        quest = Word.quest(url=self.request.GET.get('url', None), word_name=word_name, title=self.request.GET.get('title', None))
+        self.render('word.html', {"quest": quest, "word_name": word_name, "dictionaries": Dictionary.my_dictionaries()})
 
-# Unregister the rollback event handler.
-django.dispatch.dispatcher.disconnect(
-    django.db._rollback_on_exception,
-    django.core.signals.got_request_exception)
+class Dicts(BaseRequestHandler):
+    @login_required
+    def get(self):
+        self.render('dicts.html', {"dictionaries": Dictionary.public_dictionaries(), "my_dictionaries": Dictionary.my_dictionaries()})
 
+    def post(self):
+        MyDictionary.add_my_dic(url_template=self.request.POST['url_template'], title=self.request.POST['title'])
+        self.redirect('/dicts')
 
+class AddToMyDicts(BaseRequestHandler):
+    @login_required
+    def get(self, id=None):
+        MyDictionary.add_my_dic(dic_id=id)
+        self.redirect("/dicts")
+
+class RemoveFromMyDicts(BaseRequestHandler):
+    @login_required
+    def get(self, id=None):
+        mydic = MyDictionary.get_by_id(int(id))
+        if mydic:
+            mydic.delete()
+        self.redirect("/dicts")
+
+class MyDictsUpdateOrder(BaseRequestHandler):
+    def post(self, id=None):
+        if id and self.request.POST['order_priority']:
+            mydic = MyDictionary.get_by_id(int(id))
+            if mydic:
+                mydic.order_priority = int(self.request.POST['order_priority'])
+                mydic.put()
+        self.redirect("/dicts/update_order")
+
+    @login_required
+    def get(self):
+        self.render('dicts_update_order.html', {"my_dictionaries": Dictionary.my_dictionaries()})
+
+class Index(BaseRequestHandler):
+    def get(self):
+        self.render('index.html', {})
+
+class Loader(BaseRequestHandler):
+    def get(self):
+        self.render('loader.js', {}, mimetype="application/javascript")
+
+application = webapp.WSGIApplication([
+    ('/ask', Ask),
+    ('/myhome', MyHome),
+    ('/dicts/update_order/(.*)', MyDictsUpdateOrder),
+    ('/dicts/update_order', MyDictsUpdateOrder),
+    ('/dicts', Dicts),
+    ('/mydics/add/(.*)', AddToMyDicts),
+    ('/mydics/remove/(.*)', RemoveFromMyDicts),
+    ('/loader.js', Loader),
+    ('/', Index),
+], debug=_DEBUG)
 
 def main():
-  application = django.core.handlers.wsgi.WSGIHandler()
-
-  # Run the WSGI CGI handler with that application.
-  util.run_wsgi_app(application)
+    run_wsgi_app(application)
 
 if __name__ == '__main__':
-  main()
+    main()
+
